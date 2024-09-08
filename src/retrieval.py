d454ae002daacbc73a7675bf6dcf39017a3504aa
Edson Araujo <junioroteia@gmail.com>
2024-09-09 00:19:03 +0200

    retrieval minor fix

--- src/retrieval.py
+++ src/retrieval.py
@@ -144,8 +144,6 @@ if __name__ == "__main__":
     if dataset == "audioset":
         # for audioset
         for direction in ['video', 'audio']:
-            data = '/data/sls/scratch/yuangong/cav-mae/pretrained_model/datafiles/audioset/audioset_eval_5_per_class_for_retrieval_cleaned.json'
-            label_csv = '/data/sls/scratch/yuangong/cav-mae/pretrained_model/datafiles/audioset/class_labels_indices.csv'
             audio_conf = {'num_mel_bins': 128, 'target_length': 1024, 'freqm': 0, 'timem': 0, 'mixup': 0, 'dataset': dataset,
                         'mode': 'eval', 'mean': -5.081, 'std': 4.4849, 'noise': False, 'im_res': 224, 'frame_use': 5}
             r1, r5, r10, mr = eval_retrieval(model, data, audio_conf=audio_conf, label_csv=label_csv, num_class=309, direction=direction, model_type='pretrain', batch_size=100)
