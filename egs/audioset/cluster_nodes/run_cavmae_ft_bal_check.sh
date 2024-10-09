684674ba5cef4c623f5b45165c74e7bafce0aea5
edsonroteia <junioroteia@gmail.com>
2024-10-09 18:48:02 +0200

    updated check ft script to run with original MAE weights

--- egs/audioset/cluster_nodes/run_cavmae_ft_bal_check.sh
+++ egs/audioset/cluster_nodes/run_cavmae_ft_bal_check.sh
@@ -16,10 +16,14 @@ export TORCH_HOME=../../pretrained_models
 model=cav-mae-ft
 ftmode=multimodal
 
-# you can replace with any checkpoint you want, but by default, we use cav-mae-scale++
+# # you can replace with any checkpoint you want, but by default, we use cav-mae-scale++
+# cur_dir=$(pwd)
+# wget -nc https://www.dropbox.com/s/l5t5geufdy3qvnv/audio_model.21.pth?dl=1 -O cav-mae-scale++.pth
+# pretrain_path=cav-mae-scale++.pth
+
 cur_dir=$(pwd)
-wget -nc https://www.dropbox.com/s/l5t5geufdy3qvnv/audio_model.21.pth?dl=1 -O cav-mae-scale++.pth
-pretrain_path=cav-mae-scale++.pth
+wget -nc https://www.dropbox.com/s/9nlz523a5q52w86/ori_mae_11.pth?dl=1 -O IN-initial.pth
+pretrain_path=${cur_dir}/IN-initial.pth
 
 freeze_base=False
 head_lr=100 # newly initialized ft layers uses 10 times larger than the base lr
@@ -28,6 +32,8 @@ bal=None
 lr=${1:-5e-5}  # Use the first argument as lr, default to 1e-4 if not provided
 batch_size=${2:-48}  # Use the second argument as batch_size, default to 24 if not provided
 ftmode=${3:-multimodal}
+cuda_devices=${4:-0,1,2,3,4,5,6,7}
+num_workers=${5:-32}
 epoch=15
 lrscheduler_start=5
 lrscheduler_decay=0.5
@@ -53,7 +59,7 @@ label_csv=datafilles/audioset_20k/cluster_nodes/class_labels_indices.csv
 exp_dir=./exp/testmae02-${dataset}-${model}-${lr}-${lrscheduler_start}-${lrscheduler_decay}-${lrscheduler_step}-bs${batch_size}-lda${lr_adapt}-${ftmode}-fz${freeze_base}-h${head_lr}-a5-$(date +%Y%m%d_%H%M%S)
 mkdir -p $exp_dir
 
-CUDA_CACHE_DISABLE=1 python -W ignore src/run_cavmae_ft.py --model ${model} --dataset ${dataset} \
+CUDA_VISIBLE_DEVICES=${cuda_devices} CUDA_CACHE_DISABLE=1 python -W ignore src/run_cavmae_ft.py --model ${model} --dataset ${dataset} \
 --data-train ${tr_data} --data-val ${te_data} --exp-dir $exp_dir \
 --label-csv ${label_csv} --n_class 527 \
 --lr $lr --n-epochs ${epoch} --batch-size $batch_size --save_model True \
@@ -65,4 +71,4 @@ CUDA_CACHE_DISABLE=1 python -W ignore src/run_cavmae_ft.py --model ${model} --da
 --wa ${wa} --wa_start ${wa_start} --wa_end ${wa_end} --lr_adapt ${lr_adapt} \
 --pretrain_path ${pretrain_path} --ftmode ${ftmode} \
 --freeze_base ${freeze_base} --head_lr ${head_lr} \
---num-workers 32
+--num-workers ${num_workers}
