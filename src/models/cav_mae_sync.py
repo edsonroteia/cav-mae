d8f3e97712b71bfda7f387020477a8489761b94f
edsonroteia <junioroteia@gmail.com>
2024-10-28 17:41:39 +0100

    fix constrastive head extract feat

--- src/models/cav_mae_sync.py
+++ src/models/cav_mae_sync.py
@@ -644,7 +644,7 @@ class CAVMAE(nn.Module):
         if self.contrastive_heads:
             for blk in self.constrative_head_audio:
                 ca = blk(ca)
-            for blk in self.constrative_head_video:
+            for blk in self.constrative_head_visual:
                 cv = blk(cv)
         
         if self.cls_token:
