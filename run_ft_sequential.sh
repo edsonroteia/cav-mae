404540fdfc3cfba8615e933d93de2e569b15f042
edsonroteia <junioroteia@gmail.com>
2024-10-31 22:31:58 +0100

    Adapt the code to support multiple class numbers

--- run_ft_sequential.sh
+++ run_ft_sequential.sh
@@ -36,11 +36,21 @@ lr3=5e-4
 lr4=1e-4
 model_id=${2:-2776}  # Use second argument as model_id, default to 2776 if not provided
 
+# In run_cavmae_ft_sync.sh, the 15th argument is the training data, and the 16th argument is the test data, and the 17th argument is the label csv file
+# tr_data=${15:-datafilles/vggsound/cluster_nodes/vgg_train_cleaned.json}
+# te_data=${16:-datafilles/vggsound/cluster_nodes/vgg_test_cleaned.json}
+# label_csv=${17:-datafilles/vggsound/cluster_nodes/class_labels_indices_vgg.csv}
+# We should pass the data and label csv file as arguments to the script according to the number of classes
+
+tr_data=datafilles/vggsound/cluster_nodes/vgg_train_${num_classes}.json
+te_data=datafilles/vggsound/cluster_nodes/vgg_test_${num_classes}.json
+label_csv=datafilles/vggsound/cluster_nodes/class_labels_indices_vgg_${num_classes}.csv
+
 # Launch training runs in remaining panes with different model IDs
-tmux send-keys -t $SESSION:0.0 "bash egs/vggsound/cluster_nodes/run_cavmae_ft_sync.sh $lr1 48 multimodal 0,1 self_attention_cls 16 True 99999 10 finetuning_vggsound $model_id True 4 16" C-m
-tmux send-keys -t $SESSION:0.2 "bash egs/vggsound/cluster_nodes/run_cavmae_ft_sync.sh $lr2 48 multimodal 2,3 self_attention_cls 16 True 99999 10 finetuning_vggsound $model_id True 4 16" C-m
-tmux send-keys -t $SESSION:0.4 "bash egs/vggsound/cluster_nodes/run_cavmae_ft_sync.sh $lr3 48 multimodal 4,5 self_attention_cls 16 True 99999 10 finetuning_vggsound $model_id True 4 16" C-m
-tmux send-keys -t $SESSION:0.5 "bash egs/vggsound/cluster_nodes/run_cavmae_ft_sync.sh $lr4 96 multimodal 6,7 self_attention_cls 16 True 99999 10 finetuning_vggsound $model_id True 4 16" C-m
+tmux send-keys -t $SESSION:0.0 "bash egs/vggsound/cluster_nodes/run_cavmae_ft_sync.sh $lr1 48 multimodal 0,1 self_attention_cls 16 True 99999 10 finetuning_vggsound $model_id True 4 16 $tr_data $te_data $label_csv" C-m
+tmux send-keys -t $SESSION:0.2 "bash egs/vggsound/cluster_nodes/run_cavmae_ft_sync.sh $lr2 48 multimodal 2,3 self_attention_cls 16 True 99999 10 finetuning_vggsound $model_id True 4 16 $tr_data $te_data $label_csv" C-m
+tmux send-keys -t $SESSION:0.4 "bash egs/vggsound/cluster_nodes/run_cavmae_ft_sync.sh $lr3 48 multimodal 4,5 self_attention_cls 16 True 99999 10 finetuning_vggsound $model_id True 4 16 $tr_data $te_data $label_csv" C-m
+tmux send-keys -t $SESSION:0.5 "bash egs/vggsound/cluster_nodes/run_cavmae_ft_sync.sh $lr4 96 multimodal 6,7 self_attention_cls 16 True 99999 10 finetuning_vggsound $model_id True 4 16 $tr_data $te_data $label_csv" C-m
 
 # Attach to tmux session
 tmux attach-session -t $SESSION
