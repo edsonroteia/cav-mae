b9c875263dfdf23eb071299c279d31a5626506a4
edsonroteia <junioroteia@gmail.com>
2024-11-02 13:59:58 +0100

    Change the tmux windows names when running the vggsound debug ft

--- run_ft_sequential.sh
+++ run_ft_sequential.sh
@@ -39,10 +39,10 @@ echo "bash egs/vggsound/cluster_nodes/run_cavmae_ft_sync.sh $lr3 48 multimodal 4
 echo "bash egs/vggsound/cluster_nodes/run_cavmae_ft_sync.sh $lr4 48 multimodal 6,7 self_attention_cls 16 True 99999 10 finetuning_vggsound $model_id True $n_register_tokens 16 $tr_data $te_data $label_csv $num_classes $lr_scheduler"
 
 # Create windows for training runs
-tmux new-window -n "lr1" "bash egs/vggsound/cluster_nodes/run_cavmae_ft_sync.sh $lr1 48 multimodal 0,1 self_attention_cls 16 True 99999 10 finetuning_vggsound $model_id True $n_register_tokens 16 $tr_data $te_data $label_csv $num_classes $lr_scheduler"
-tmux new-window -n "lr2" "bash egs/vggsound/cluster_nodes/run_cavmae_ft_sync.sh $lr2 48 multimodal 2,3 self_attention_cls 16 True 99999 10 finetuning_vggsound $model_id True $n_register_tokens 16 $tr_data $te_data $label_csv $num_classes $lr_scheduler"
-tmux new-window -n "lr3" "bash egs/vggsound/cluster_nodes/run_cavmae_ft_sync.sh $lr3 48 multimodal 4,5 self_attention_cls 16 True 99999 10 finetuning_vggsound $model_id True $n_register_tokens 16 $tr_data $te_data $label_csv $num_classes $lr_scheduler"
-tmux new-window -n "lr4" "bash egs/vggsound/cluster_nodes/run_cavmae_ft_sync.sh $lr4 48 multimodal 6,7 self_attention_cls 16 True 99999 10 finetuning_vggsound $model_id True $n_register_tokens 16 $tr_data $te_data $label_csv $num_classes $lr_scheduler"
+tmux new-window -n "${lr1} ${model_id}" "bash egs/vggsound/cluster_nodes/run_cavmae_ft_sync.sh $lr1 48 multimodal 0,1 self_attention_cls 16 True 99999 10 finetuning_vggsound $model_id True $n_register_tokens 16 $tr_data $te_data $label_csv $num_classes $lr_scheduler"
+tmux new-window -n "${lr2} ${model_id}" "bash egs/vggsound/cluster_nodes/run_cavmae_ft_sync.sh $lr2 48 multimodal 2,3 self_attention_cls 16 True 99999 10 finetuning_vggsound $model_id True $n_register_tokens 16 $tr_data $te_data $label_csv $num_classes $lr_scheduler"
+tmux new-window -n "${lr3} ${model_id}" "bash egs/vggsound/cluster_nodes/run_cavmae_ft_sync.sh $lr3 48 multimodal 4,5 self_attention_cls 16 True 99999 10 finetuning_vggsound $model_id True $n_register_tokens 16 $tr_data $te_data $label_csv $num_classes $lr_scheduler"
+tmux new-window -n "${lr4} ${model_id}" "bash egs/vggsound/cluster_nodes/run_cavmae_ft_sync.sh $lr4 48 multimodal 6,7 self_attention_cls 16 True 99999 10 finetuning_vggsound $model_id True $n_register_tokens 16 $tr_data $te_data $label_csv $num_classes $lr_scheduler"
 
 # Command that will be run in each window if default values are used
 # bash egs/vggsound/cluster_nodes/run_cavmae_ft_sync.sh 1e-2 48 multimodal 4,5,6,7 self_attention_cls 32 True 99999 10 finetuning_vggsound 2918 True 8 16 datafilles/vggsound/cluster_nodes/vgg_train_20.json datafilles/vggsound/cluster_nodes/vgg_test_20.json datafilles/vggsound/cluster_nodes/class_labels_indices_vgg_20.csv;
